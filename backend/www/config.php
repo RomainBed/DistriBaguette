5a5348787364faf896cc18fa09e46c1de7942a07
CastagnetFlorian <98601741+CastagnetFlorian@users.noreply.github.com>
2022-03-10 11:39:34 +0100

    Add files via upload

--- backend/www/config.php
+++ backend/www/config.php
@@ -3,4 +3,6 @@ $host = "172.20.233.53/BDD";
 $user = "client";
 $password = "client";
 $table = "distri_baguette"; 
+//$name= $_POST['name'];
+//$lieu= $_POST['lieu'];
 ?>
--- backend/www/index.php
+++ backend/www/index.php
@@ -5,19 +5,51 @@ include 'config.php';
 	require './lib/hyla_tpl.class.php';
 	
 //connection à la base de données 
+	
 	$id=mysqli_connect($host,$user,$password,$table) or "Erreur de connexion";
+		if($con->mysqli_connect_errno){
+			
+		} else {
+			echo "Error connection";
+		};
+		
+
+	//$con=mysqli_query($id,$sql);
+//Test selection
 	
-	$sql= "SELECT * FROM 'boulanger'";
+	//$sql= $con->("SELECT * FROM 'boulanger'");
+
 	
-	$test=mysqli_query($id,$sql);	
+//Intégration de la bibliothèque Hyla_Tpl
+
 	$tpl = new Hyla_Tpl('tpl');
 	$tpl->importFile('page_web.html');
-	
-	$i = 1;
-	while ($i <= 10) {
+
+//Distributeur
+
+	$Distributeur = 1;	
+	//Nombre de distributeur
+	while ($Distributeur <= 10) {
+
+		//$users= $con->query("SELECT * FROM users");
+		
+		//echo $user['name'];
+		//echo $lieu[''];
+		
+		$Distributeur++;
+		//rendu du bloc distrib
 		$tpl->render('distrib');
-		echo $i++;
 	}		
 	
 	echo $tpl->render();
+	
+	/*
+	//Ajouter valeur dans la table depuis html
+	$post= $con->query("INSERT INTO users VALUES(DEFAULT,'$name','$lieu')");
+	if($post) {
+		header("location:page_web.html");
+	} else {
+		echo "Erreur create";
+	}
+	*/
 ?>
