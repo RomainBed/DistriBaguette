5f59830c0191dc0ebfabadb1f57b64a889432359
RomainBed <98598157+RomainBed@users.noreply.github.com>
2022-03-11 16:47:43 +0100

    Add files via upload

---
+++ FrontEnd/Message_PHP/msgfr.php
@@ -0,0 +1,26 @@
+<?php
+//Variable dans la page d'accueil
+$page = "DistriBaguette";
+$title = "NOS DISTRIBUTEURS";
+$nom_distri = "Distributeur 1";
+$desc = "DESCRIPTION";
+$data = "Affichage des données";
+$footertitle = "DistriBaguette";
+$projet = "Projet 2022";
+
+//Prenom dans le footer
+$nom1 = "Romain";
+$nom2 = "Florian";
+$nom3 = "Anthony";
+$nom4 = "Younes";
+
+//Tableau de donnés
+$stock = "STOCK";
+$etat = "ETAT";
+$loca = "LOCALISATION";
+$boulanger = "BOULANGER";
+
+//service
+$admin = "ADMINISTRATION";
+$connect = "Connexion";
+?>
