69e57c82381a878253ac22ddfff9528b368da975
CastagnetFlorian <98601741+CastagnetFlorian@users.noreply.github.com>
2022-03-11 13:53:07 +0100

    Add files via upload

---
+++ backend/www/TU/connection.php
@@ -0,0 +1,14 @@
+<?php
+//introduction du fichier config.php
+include 'config.php';
+//------------------------------------CONNECTION---------------------------------------
+//connection à la base de données 
+	$con= mysqli_connect($host,$user,$password,$base,3306);
+		if ($con->connect_errno) {
+			
+			error_log('Connection erreur: ' . $con->connect_errno);
+		}
+	// $con=mysqli_query($con,$sql);
+// ---------------------------------------------------------------------------
+
+?>
