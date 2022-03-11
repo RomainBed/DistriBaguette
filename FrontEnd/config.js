2bf7ffeffc084f07f220e562d3bf830e5bc7798a
RomainBed <98598157+RomainBed@users.noreply.github.com>
2022-03-11 09:33:26 +0100

    Add files via upload

---
+++ FrontEnd/config.js
@@ -0,0 +1,20 @@
+//Variable dans la page d'accueil
+var title = "NOS DISTRIBUTEURS";
+var nom_distri = "Distributeur 1";
+var desc = "DESCRIPTION";
+var data = "Affichage des données";
+var footertitle = "DistriBaguette";
+var projet = "Projet 2022";
+
+//Prenom dans le footer
+var nom1 = "Romain"
+var nom2 = "Florian"
+var nom3 = "Anthony"
+var nom4 = "Younes"
+
+//Tableau de donnés
+var stock = "STOCK";
+var etat = "ETAT";
+var loca = "LOCALISATION"
+var boulanger = "BOULANGER"
+
