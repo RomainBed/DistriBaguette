3d20b21bee41f2ac40de026b6ef5daf3f5d892bd
anthoreno <98892002+anthoreno@users.noreply.github.com>
2022-03-18 08:04:16 +0100

    Add files via upload

--- backend/BDD/distribaguette(2).sql
+++ backend/BDD/distribaguette(2).sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le :  jeu. 10 mars 2022 à 09:44
--- Version du serveur :  5.7.17
--- Version de PHP :  5.6.30
+-- Client :  localhost:3306
+-- Généré le :  Jeu 17 Mars 2022 à 14:26
+-- Version du serveur :  10.3.31-MariaDB-0+deb10u1
+-- Version de PHP :  7.3.31-1~deb10u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,19 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `boulanger` (
-  `ID_Boulanger` int(11) UNSIGNED NOT NULL,
-  `Nom` varchar(20) NOT NULL,
-  `Prénom` varchar(20) NOT NULL,
-  `Adresse_Mail` varchar(30) NOT NULL,
-  `Téléphone` int(10) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_boulanger` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `adresse_mail` varchar(40) NOT NULL,
+  `telephone` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `boulanger`
+-- Contenu de la table `boulanger`
 --
 
-INSERT INTO `boulanger` (`ID_Boulanger`, `Nom`, `Prénom`, `Adresse_Mail`, `Téléphone`) VALUES
-(1, 'DOUFFET', 'André', 'a.douffet@gmail.com', 658742349),
+INSERT INTO `boulanger` (`id_boulanger`, `nom`, `prenom`, `adresse_mail`, `telephone`) VALUES
+(1, 'DOUFFET', 'Andre', 'a.douffet@gmail.com', 658742349),
 (2, 'LANGLET', 'Eric', 'e.langlet@gmail.com', 612478753),
 (3, 'AUBET', 'Maurenne', 'm.aubet@gmail.com', 745896321),
 (4, 'MONTY', 'Christian', 'm.monty@gmail.com', 744953266),
@@ -52,6 +50,24 @@ INSERT INTO `boulanger` (`ID_Boulanger`, `Nom`, `Prénom`, `Adresse_Mail`, `Tél
 (9, 'PLADOT', 'Didier', 'd.pladot@gmail.com', 674321985),
 (10, 'NOUNIER', 'Arthur', 'a.nounier@gmail.com', 767663955);
 
+-- AUTO_INCREMENT pour la table `boulanger`
+--
+ALTER TABLE `boulanger`
+  MODIFY `id_boulanger` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+-- Index pour la table `boulanger`
+--
+ALTER TABLE `boulanger`
+  ADD PRIMARY KEY (`id_boulanger`),
+  ADD UNIQUE KEY `nom` (`nom`),
+  ADD UNIQUE KEY `prenom` (`prenom`),
+  ADD UNIQUE KEY `adresse_mail` (`adresse_mail`),
+  ADD UNIQUE KEY `telephone` (`telephone`);
+  
+  -- Contraintes pour la table `boulanger`
+
+ALTER TABLE `boulanger`
+  ADD CONSTRAINT `CHK2` CHECK (`nom` != `` AND `prenom` != `` AND `adresse_mail` != `` );
 -- --------------------------------------------------------
 
 --
@@ -59,74 +75,48 @@ INSERT INTO `boulanger` (`ID_Boulanger`, `Nom`, `Prénom`, `Adresse_Mail`, `Tél
 --
 
 CREATE TABLE `distributeur` (
-  `ID_Distributeur` int(10) UNSIGNED NOT NULL,
-  `Nom` varchar(20) NOT NULL,
-  `Localisation` varchar(30) NOT NULL,
-  `Stock` int(10) UNSIGNED NOT NULL,
-  `Etat` enum('0','1','2') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_distributeur` int(10) UNSIGNED NOT NULL,
+  `nom` varchar(30) NOT NULL,
+  `localisation` varchar(30) NOT NULL,
+  `stock` int(10) UNSIGNED NOT NULL,
+  `etat` enum('0','1','2') NOT NULL
+) ;
 
 --
--- Déchargement des données de la table `distributeur`
+-- Contenu de la table `distributeur`
 --
 
-INSERT INTO `distributeur` (`ID_Distributeur`, `Nom`, `Localisation`, `Stock`, `Etat`) VALUES
-(1, 'Ditri Mairie', 'Mairie Angers', 60, '1'),
-(2, 'Distri Parc', 'Parc Angers', 60, '1'),
-(3, 'Distri Stade', 'Stade Angers', 60, '1'),
-(4, 'Distri Piscine', 'Piscine Angers', 60, '1'),
-(5, 'Distri Centre', 'Centre Commercial', 60, '1'),
-(6, 'Distri raliemment', 'Place du Raliemment', 60, '1'),
-(7, 'Distri Gymnase', 'Gymnase Angers', 60, '1'),
-(8, 'Distri Carburant', 'Station Service Nord', 60, '1'),
-(9, 'Distri Cinéma', 'Cinéma Pathé Angers', 60, '1'),
-(10, 'Distri Bowling', 'Bowling Angers', 60, '1');
+INSERT INTO `distributeur` (`id_distributeur`, `nom`, `localisation`, `stock`, `etat`) VALUES
+(1, 'Ditri Mairie', 'Mairie Angers', 59, '1'),
+(2, 'Distri Parc', 'Parc Angers', 58, '0'),
+(3, 'Distri Stade', 'Stade Angers', 57, '2'),
+(4, 'Distri Piscine', 'Piscine Angers', 56, '1'),
+(5, 'Distri Centre', 'Centre Commercial', 55, '1'),
+(6, 'Distri raliemment', 'Place du Raliemment', 54, '2'),
+(7, 'Distri Gymnase', 'Gymnase Angers', 53, '1'),
+(8, 'Distri Carburant', 'Station Service Nord', 52, '1'),
+(9, 'Distri Cinema', 'Cinema Pathe Angers', 51, '0'),
+(10, 'Distri Bowling', 'Bowling Angers', 50, '1');
 
---
--- Index pour les tables déchargées
---
 
---
--- Index pour la table `boulanger`
---
-ALTER TABLE `boulanger`
-  ADD PRIMARY KEY (`ID_Boulanger`),
-  ADD UNIQUE KEY `Adresse_Mail` (`Adresse_Mail`),
-  ADD UNIQUE KEY `Nom` (`Nom`),
-  ADD UNIQUE KEY `Téléphone` (`Téléphone`);
-
---
 -- Index pour la table `distributeur`
 --
 ALTER TABLE `distributeur`
-  ADD PRIMARY KEY (`ID_Distributeur`),
-  ADD UNIQUE KEY `Nom` (`Nom`),
-  ADD UNIQUE KEY `Localisation` (`Localisation`);
+  ADD PRIMARY KEY (`id_distributeur`),
+  ADD UNIQUE KEY `nom` (`nom`),
+  ADD UNIQUE KEY `localisation` (`localisation`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `boulanger`
---
-ALTER TABLE `boulanger`
-  MODIFY `ID_Boulanger` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
 -- AUTO_INCREMENT pour la table `distributeur`
 --
 ALTER TABLE `distributeur`
-  MODIFY `ID_Distributeur` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
---
--- Contraintes pour les tables déchargées
---
+  MODIFY `id_distributeur` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
---
 -- Contraintes pour la table `distributeur`
 --
 ALTER TABLE `distributeur`
-  ADD CONSTRAINT `FK_Distributeur` FOREIGN KEY (`ID_Distributeur`) REFERENCES `boulanger` (`ID_Boulanger`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
+  ADD CONSTRAINT `fk` FOREIGN KEY (`id_distributeur`) REFERENCES `boulanger` (`id_boulanger`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `distributeur`
+  ADD CONSTRAINT `CHK` CHECK (`nom` != `` AND `localisation` != ``);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
