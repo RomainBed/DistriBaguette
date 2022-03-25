16d10897562d8bc73c8491566288a51703ca7dcf
CastagnetFlorian <98601741+CastagnetFlorian@users.noreply.github.com>
2022-03-25 16:46:48 +0100

    Add files via upload

--- backend/www/TU/login_TU.php
+++ backend/www/TU/login_TU.php
@@ -1,41 +1,46 @@
 <?php
+session_start();
+
 //Intégration de la bibliothèque Hyla_Tpl
 
 	require './lib/hyla_tpl.class.php';
 	require './php/msgfr.php';
-	require('config.php');
-
+	require './config.php';
+	
 	$tpl = new Hyla_Tpl('html');
 	$tpl->importFile('service.html');
 	
 	$tpl->setVar('A_page', $A_page);
-	$tpl->setVar('flootertitle',$A_footertitle);
+	$tpl->setVar('A_footertitle', $A_footertitle);
 	$tpl->setVar('A_projet',$A_projet);
 	$tpl->setVar('S_admin',$S_admin);
 	$tpl->setVar('S_connect',$S_connect);
 	
-require('config.php');
-session_start();
-$tpl->render('connexion');
+	if ( @$_POST['submit'] == "Login" ){
+		
+	  $username = stripslashes($_POST['username']);
+	  //$username = PDO::quote($pdo, $username);
+	  
+	  $password = stripslashes($_POST['password']);
+	  //$password = PDO::quote($pdo, $password)
+	  
+	  try {
+		  //$request = new PDO("mysql -u '$username' -p '$password' -h '172.20.233.109' -D 'distribaguette'");
+		  //$_SESSION['pdo'] = new PDO("mysql:distribaguette;host=172.20.233.109", $username, $password);
+		  //$pdo = new PDO("mysql:host=172.20.233.109;dbname=distribaguette", $username, $password);
+		  $pdo = new PDO("mysql:host=172.20.233.109;dbname=distribaguette", $username, $password);
 
-	if (isset($_POST['username'])){
-	  $username = stripslashes($_REQUEST['username']);
-	  $username = mysqli_real_escape_string($pdo, $username);
-	  $password = stripslashes($_REQUEST['password']);
-	  $password = mysqli_real_escape_string($pdo, $password);
-		$query = "SELECT * FROM `users` WHERE username='$username' and password='$password.'";
-	  $result = mysqli_query($pdo,$query) or die(mysql_error());
-	  $rows = mysqli_num_rows($result);
-	  if($rows==1){
-		  $_SESSION['username'] = $username;
-		  header("Location: index_admin.php");
-	  }else{
-		$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
-	  }
+		  if ( $pdo ) {
+			   $_SESSION['username'] = $username;
+			   $_SESSION['password'] = $password;
+
+			  header("Location: index_admin.php");
+			}
+		}
+	  catch(PDOexception $e) {
+		echo $e->getMessage();
+		}
 	}
-	  // if (isset($_POST['username'])){
-      // $_POST['username'] = $username;
-      // header("Location: index_admin.php");
-// }
+	  
 	echo $tpl->render();
 ?>
