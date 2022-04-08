a0399a526523e763b0b884ae6a7bdabe405f2329
CastagnetFlorian <98601741+CastagnetFlorian@users.noreply.github.com>
2022-04-08 10:45:06 +0200

    Delete index_param_admin.php

--- backend/www/Requette_SQL_pour_PHP/index_param_admin.php
+++
@@ -1,3 +0,0 @@
-$param_boulanger="INSERT INTO boulanger(nom, prenom, telephone, adresse_mail) VALUES ('$nom_boulanger', '$prenom', '$telephone', '$mail');"
-
-$param_distributeur="INSERT INTO distributeur(nom, localisation, stock, etat) VALUES ('$nom_distri', '$localisation', '$stock', '$etat');
